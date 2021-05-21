d2ec336106836df4dd8f22941e12e7c285a34446
Allan Vasconcelos <asv@cin.ufpe.br>
2021-05-21 20:15:28 -0300

    Updated BFS.cpp, added isBipartite function

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -57,15 +57,69 @@ void bfs(){
 
 }
 
+// This function return true with a graph is bipartite
+bool isBipartite(){
+
+    // Store color of the vertex (-1 = unvisited, 0 = black, 1 = whites)
+    vector<int> color(N, -1);
+ 
+    // queue for BFS storing {vertex , colour}
+    queue<pair<int, int> > q;
+   
+    //loop incase graph is not connected
+    for (int i = 0; i < N; i++) {
+       
+        //if not coloured (not visited)
+        if (color[i] == -1) {
+           
+            // Assign any color
+            q.push({ i, 0 });
+            color[i] = 0;
+
+            //BFS
+            while (!q.empty()) {
+
+                pair<int, int> p = q.front();
+                q.pop();
+               
+                //current vertex
+                int v = p.first;
+                //colour of current vertex
+                int c = p.second;
+                 
+                //traversing vertexes connected to current vertex
+                for (int j : adj[v]) {
+                   
+                    // Can't be bipartite
+                    if (color[j] == c)
+                        return false;
+                   
+                    //if uncooloured (unvisited)
+                    if (color[j] == -1) {
+                        //colouring with opposite color to that of parent
+                        color[j] = 1-c;
+                        q.push({ j, col[j] });
+                    }
+                }
+            }
+        }
+    }
+
+    // Graph is Bipartite
+    return true;
+}
+
 /*
 
 Time Complexity
 
 addEdge     -> O(1)
 bfs         -> O(V+E)
+isBipartite -> O(V+E)
 
 Links:
 
 https://www.geeksforgeeks.org/bfs-using-stl-competitive-coding/
+https://www.geeksforgeeks.org/bipartite-graph/
 
 */
