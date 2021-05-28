4e918ac61dabb228dc2d83c4f3ed9d2bfaf13625
Allan Vasconcelos <asv@cin.ufpe.br>
2021-05-28 10:12:39 -0300

    Updated Prim.cpp

--- Graph/MST/Prim.cpp
+++ Graph/MST/Prim.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 typedef long long ll;
-const int N = 10; // Number of vertex
+const int N = 1e5+10; // Number of vertex
 
 // Graph inplementation using Adjacency List ( adj[u] -> (v,w) ) (0-Based)
 vector<pair<int,ll>> adj[N];
@@ -25,8 +25,9 @@ ll prim(){
     // Cost to build the MST
     ll wMST = 0;
 
-    // Create a vector for costs and initialize all costs as infinite
+    // Create a vector for costs and initialize all costs as infinite and source as 0
     vector<ll> costs(N, LONG_LONG_MAX);
+    costs[src] = 0;
  
     // To store parent array which in turn store MST
     vector<int> parent(N, -1);
@@ -34,65 +35,43 @@ ll prim(){
     // To keep track of vertices included in MST
     vector<bool> inMST(N, false);
  
-    // Insert source in priority queue and initialize its costs as 0 (w,u)
+    // Insert source in priority queue (w,u)
     pq.emplace(0, src);
-    costs[src] = 0;
 
     while (!pq.empty()){
 
         // Extracting the minimum costs vertex
         int u = pq.top().second;
+        ll wt = pq.top().first;
         pq.pop();
 
-        // If the vertex hasn't been added, it is necessary to add the cost to reach it
-        if(inMST[u] == false){
-            wMST += costs[u];
-        }
+        // If the vertex has already been added there is no need to continue
+        if(inMST[u] == true) continue;
 
         // Include vertex in MST (Took the shortest path possible to get to it)
         inMST[u] = true;
+        wMST += wt;
 
         // Get all adjacent vertices of "u"
         for(auto i : adj[u]){
             int v = i.first;
             ll w = i.second;
-
+            
             // "v" isn't in MST and weight of (u,v) is smaller than current costs of "v"
             if(inMST[v] == false && costs[v] > w){
                 // Updating weight to "v" (Is a possible edge that i can put in the MST)
                 costs[v] = w;
-                pq.emplace(costs[v],v);
+                pq.emplace(w,v);
                 parent[v] = u;
             }
         }
     }
     
     /*
-
     // Print edges of MST using parent array (We start from 1 because the parent of the source is -1)
-
     for (int i = 1; i < N; ++i)
         printf("%d - %d\n", parent[i], i);
-
     */ 
    
     return wMST;
 }
-
-/*
-
-Time Complexity
-
-prim    -> O(E*logN)
-
-Links:
-
-https://www.geeksforgeeks.org/prims-algorithm-using-priority_queue-stl/
-https://cp-algorithms.com/graph/mst_prim.html
-
-Obs.: 
-
-1º) Para criar uma Maximum Spanning Tree utilizamos uma Max Heap (priority queue), 
-definimos o infinito como "LONG_LONG_MIN" e trocamos a linha 61 para "if(inMST[v] == false && costs[v] < w)"
-
-*/
