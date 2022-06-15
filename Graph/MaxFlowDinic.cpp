d5654b5ac406fd725c16fdc992f5d8d39b6b40ea
Allan Soares Vasconcelos-ALLSOVAS <asv@cin.ufpe.br>
2022-06-14 23:00:58 -0300

    Update

--- Graph/MaxFlowDinic.cpp
+++ Graph/MaxFlowDinic.cpp
@@ -1,91 +1,157 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+#define int long long
+using ll = long long;
 
-// Struct to represent the Residual Network
-struct FlowEdge {
-    int u,v;
-    ll cap, flow = 0;
-    FlowEdge (int u, int v, ll cap) : u(u), v(v), cap(cap) {}
-};
+const ll INF = 1e9 + 7;
 
 // Dinic's Algorithm to find the Max Flow of a graph
-struct Dinic {
-    const ll flow_inf = 1e18;
-    vector<FlowEdge> edges;
-    vector<vector<int>> adj;    // Graph inplementation using Adjacency List, here will be saved the index for mapping in "edges"
-    int n, m = 0;               // n = number of vertices and m = number of edges
-    int s, t;                   // s = source and t = sink
-    vector<int> level, ptr;
-    queue<int> q;
+class Dinic{
+    int N;
+    vector<ll> level;
+    vector<bool> dead;
 
-    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
-        adj.resize(n);
-        level.resize(n);
-        ptr.resize(n);
+public:
+
+    struct Edge{
+        Edge(int vertice, ll capacity){
+            v = vertice;
+            cap = capacity;
+        }
+        ll v;
+        ll cap;
+    };
+    int source;
+    int sink;
+    vector<Edge> edge;
+    vector<vector<int>> g;
+
+    Dinic(int size, int u, int v){  // Initializing Dinic
+        g.resize(size);
+        N = size;
+        level.resize(size);
+        source = u;
+        sink = v;
     }
 
-    void add_edge(int u, int v, ll cap){
-        edges.emplace_back(u, v, cap);
-        edges.emplace_back(v, u, 0);
-        adj[u].push_back(m);
-        adj[v].push_back(m + 1);
-        m += 2;
+    void addEdge(int u, int v, ll cap){ // Making Residual Network
+        g[u].push_back(edge.size());    
+        edge.push_back(Edge(v,cap));
+        g[v].push_back(edge.size());
+        edge.push_back(Edge(u,0));
     }
 
-    // Construct the Augmenting Level Path
-    bool bfs(){
-        while (!q.empty()){
+    ll run(){
+
+        ll flow = 0;
+        while(BFS())
+            flow += maxflow(source, INF);
+        
+        return flow;
+    }
+
+    // Algorithm to find the edges from MinCut by dividing the graph into 2 subgraphs.
+    void mincut(){
+
+        set<int> reachable; // 2 subgraphs
+        set<int> unreachable;
+
+        vector<bool> visited(N, false);
+        queue<int> q;
+        q.push(source);
+        reachable.insert(source);
+
+        while(!q.empty()){  // BFS to find all vertices that are reached by the source using positive-capacity edges
+
+            int u = q.front();
+            visited[u] = true;
+            q.pop();
+
+            for(auto x: g[u]){
+                if(!visited[edge[x].v] && edge[x].cap > 0){
+                    q.push(edge[x].v);
+                    reachable.insert(edge[x].v);
+                }
+            }
+        }
+        
+        for(int i = 1; i < N; i++){ // If the graph is 0-based you need to put i = 0
+            if(reachable.find(i) == reachable.end())
+                unreachable.insert(i);
+        }
+
+        bool flag = true;
+        for(auto i: reachable){ // Printing edges responsible for connecting the 2 subgraphs
+            for(auto j: g[i]){
+                if(unreachable.find(edge[j].v) != unreachable.end()){
+                    if(flag == true)cout << i << " " << edge[j].v << '\n';
+                    flag = flag^true;
+                }
+            }
+        }
+    }
+
+    // Algorithm to find edges from Maximum Matching of a bipartite graph (may have variations which capacity need not be 0)
+    void max_matching(int n, int m){
+
+        for(int i = 1; i <= n + m; i++){
+            for(auto j: g[i]){
+                if(j%2 == 0 && edge[j].cap == 0){
+                    if(edge[j].v != 0 && edge[j].v != n+m+1)
+                        cout << i << " " << edge[j].v - n << '\n';
+                }
+            }
+        }
+    }
+
+private:
+    
+    bool BFS(){ // Construct the Augmenting Level Path
+
+        for(int i = 0; i < N; i++) level[i] = INF;
+        dead.clear();
+        dead.resize(N, false);
+        level[source] = 0;
+        queue<int> q;
+
+        q.push(source);
+        while(!q.empty()){
+
             int u = q.front();
             q.pop();
-            for (int id : adj[u]){
-                if (edges[id].cap - edges[id].flow < 1)  // This edge is already using maximum flow
-                    continue;
-                if (level[edges[id].v] != -1)            // I've already visited this vertex
-                    continue;
-                level[edges[id].v] = level[u] + 1;
-                q.push(edges[id].v);
+
+            if(u == sink) return true;
+
+            for(auto x: g[u]){
+                if(level[edge[x].v] == INF && edge[x].cap > 0){
+                    level[edge[x].v] = level[u] + 1;
+                    q.push(edge[x].v);
+                }
             }
         }
-        return level[t] != -1;
+        return false;
     }
 
-    // Find path Source -> Sink
-    ll dfs(int u, ll pushed){
-        if (pushed == 0)
-            return 0;
-        if (u == t)
-            return pushed;
-        for (int& cid = ptr[u]; cid < (int)adj[u].size(); cid++){
-            int id = adj[u][cid];
-            int v = edges[id].v;
-            if (level[u] + 1 != level[v] || edges[id].cap - edges[id].flow < 1)
-                continue;
-            ll tr = dfs(v, min(pushed, edges[id].cap - edges[id].flow));
-            if (tr == 0)
-                continue;
-            edges[id].flow += tr;
-            edges[id ^ 1].flow -= tr;
-            return tr;
-        }
-        return 0;
-    }
+    ll maxflow(int u, ll flow){
 
-    ll flow(){
-        ll maxFlow = 0;
-        while (true){
-            fill(level.begin(), level.end(), -1);
-            level[s] = 0;
-            q.push(s);
-            if (!bfs())
-                break;
-            fill(ptr.begin(), ptr.end(), 0);
-            while (ll pushed = dfs(s, flow_inf)){
-                maxFlow += pushed;
-            }
+        if(dead[u] || flow == 0) return 0;
+        if(u == sink) return flow;
+
+        ll answer = 0;
+
+        for(auto i: g[u]){
+            if(level[edge[i].v] != level[u] + 1) continue;
+            ll f = maxflow(edge[i].v, min(edge[i].cap, flow));
+            int reversed_i = (i%2 == 0 ? i+1 : i-1);    // Finding the "even" edge of "i"
+            flow -= f;
+            answer += f;
+            edge[i].cap -= f;
+            edge[reversed_i].cap += f;
         }
-        return maxFlow;
+
+        if(answer == 0) dead[u] = true;
+        return answer;
     }
 };
 
@@ -93,7 +159,9 @@ struct Dinic {
 
 Time Complexity
 
-Dinic   -> O(V²E)
+Dinic                               -> O(V²E)
+Dinic Maximum Matching              -> O(E*sqrt(V))
+Dinic in Unit Capacities Networks   -> O(E*V^(2/3))
 
 Links:
 
