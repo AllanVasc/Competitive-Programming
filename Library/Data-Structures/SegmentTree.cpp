44c4bb42268c1c32d9817d82648bd34168c8284b
Allan Soares Vasconcelos <allsovas@motorola.com>
2022-11-11 07:50:47 -0300

    Refactoring

--- Library/Data-Structures/SegmentTree.cpp
+++ Library/Data-Structures/SegmentTree.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-const int MAX = 20000; // Size of v (Array used to construct)
+#define int long long
+const int ms = 20000; // Max size of v (Array used to construct)
 
 // Implementation of a Recursive Segment Tree (1-Based) (Sum Operation)
-ll segTree[4*MAX];
+int segTree[4*ms];
 
 // Array used to construct Segtree (Need to be 1-Based)
-vector<ll> v;
+vector<int> v;
 
 // SINGLE-ELEMENT MODIFY, RANGE QUERY [l,r]
 
@@ -20,16 +20,15 @@ void build(int p, int l, int r){
         segTree[p] = v[r];
     }
     else{
-
         int m = (l+r)/2;
         int lc = 2*p;
         int rc = lc + 1;
 
         // Building Left Children
-        build(lc,l,m);
+        build(lc, l, m);
 
         // Building Rigth Children
-        build(rc,m+1,r);
+        build(rc, m + 1, r);
         
         // Building Node (Sum Operation) 
         segTree[p] = segTree[lc] + segTree[rc];
@@ -38,63 +37,48 @@ void build(int p, int l, int r){
 }
 
 // Call using update(1,1,N,idx,value) (idx need to be 1-Based)
-void update(int p,int l, int r, int idx, ll value){
+void update(int p,int l, int r, int idx, int value){
 
     // Updating Leaf
     if(l == r){
         segTree[p] = value;
     }
     else{
-
         int m = (l+r)/2;
         int lc = 2*p;
         int rc = lc + 1;
 
         // Modified Leaf is on the left.
-        if(idx <= m){
+        if(idx <= m) update(lc, l, m, idx, value);
 
-            update(lc,l,m,idx,value);
-        }
         // Modified Leaf is on the rigth.
-        else{
-
-            update(rc,m+1,r,idx,value);
-        }
+        else update(rc, m + 1, r, idx, value);
 
         // Update Node
         segTree[p] = segTree[lc] + segTree[rc];
-
     }
 }
 
 // Call using query(1,1,N,ql,qr) (ql and qr need to be 1-Based)
-ll query(int p, int l, int r, int ql, int qr){
+int query(int p, int l, int r, int ql, int qr){
 
     // This node is inside the range answer
     if(ql <= l && r <= qr){
         return segTree[p];
     }
     else{
-
         int m = (l+r)/2;
         int lc = 2*p;
         int rc = lc + 1;
 
         // Our answer is just in the Left Children
-        if(qr <= m){
+        if(qr <= m) return query(lc, l, m, ql, qr);
 
-            return query(lc,l,m,ql,qr);
-        }
         // Our answer is just in the Rigth Children
-        else if(ql > m){
+        else if(ql > m) return query(rc, m + 1, r, ql, qr);
 
-            return query(rc,m+1,r,ql,qr);
-        }
-        else{
-
-            // Our answer is an intersection of the 2 sides
-            return query(lc,l,m,ql,qr) + query(rc,m+1,r,ql,qr);
-        }
+        // Our answer is an intersection of the 2 sides
+        else return query(lc, l, m, ql, qr) + query(rc, m + 1, r, ql, qr);
     }
 }
 
@@ -113,11 +97,11 @@ void updateRange(int p, int l, int r, int ql, int qr){
             updateRange(lc, l, m, ql, qr);
         }
         else if(ql > m){
-            updateRange(rc, m+1, r, ql, qr);
+            updateRange(rc, m + 1, r, ql, qr);
         }
         else{
-            updateRange(lc,l,m,ql,qr);
-            updateRange(rc,m+1,r,ql,qr);
+            updateRange(lc, l, m, ql, qr);
+            updateRange(rc, m + 1, r, ql, qr);
         }
         segTree[p] = segTree[lc] + segTree[rc];
     }
