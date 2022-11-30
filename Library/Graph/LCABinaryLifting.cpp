7586628bc479fdf29f7ce0f30458ededfb602c3f
Allan Soares Vasconcelos <allsovas@motorola.com>
2022-11-30 20:53:45 -0300

    Adding some Theorems

--- Library/Graph/LCABinaryLifting.cpp
+++ Library/Graph/LCABinaryLifting.cpp
@@ -6,18 +6,18 @@ typedef long long ll;
 const int MAXN = 10000;  	// Number of vertices
 const int LOG = 20;     	// Log2(N)
 
-vector<vector<int>> children;	// Graph inplementation using Adjacency List (0-Based)
-int up[MAXN][LOG];            	// up[v][j] is 2^j-th ancestor of v
+vector<vector<int>> g;	// Graph inplementation using Adjacency List (0-Based)
+int up[MAXN][LOG];  	// up[v][j] is 2^j-th ancestor of v
 int depth[MAXN];
 
 // Preprocessing
 void dfs(int a, int p) {
-	for(int b : children[a]) {
+	for(int b : g[a]) {
 		if(b == p) continue;    // don't go back to the father
 		depth[b] = depth[a] + 1;
 		up[b][0] = a; 			// a is parent of b
 		for(int j = 1; j < LOG; j++) {
-			up[b][j] = up[ up[b][j-1] ][j-1];
+			up[b][j] = up[ up[b][j - 1] ][j - 1];
 		}
 		dfs(b, a);
 	}
@@ -25,9 +25,7 @@ void dfs(int a, int p) {
 
 // Algorithm to find the Lowest Common Ancestor using Binary Lifting
 int lca(int a, int b) {
-	if(depth[a] < depth[b]) {
-		swap(a, b);
-	}
+	if(depth[a] < depth[b]) swap(a, b);
 	// 1) Get same depth.
 	int k = depth[a] - depth[b];
 	for(int j = LOG - 1; j >= 0; j--) {
@@ -36,9 +34,7 @@ int lca(int a, int b) {
 		}
 	}
 	// 2) if b was ancestor of a then now a == b
-	if(a == b) {
-		return a;
-	}
+	if(a == b) return a;
 	// 3) move both a and b with powers of two
 	for(int j = LOG - 1; j >= 0; j--) {
 		if(up[a][j] != up[b][j]) {
@@ -53,8 +49,8 @@ int lca(int a, int b) {
 
 Time Complexity
 
-lca     -> O(logN) 
 dfs     -> O(N*logN)
+lca     -> O(logN) 
 
 Links:
 
