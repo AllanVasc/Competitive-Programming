de47a026a9be86d7baa4b9003b01d6f54a7021c6
Allan Soares Vasconcelos <allsovas@motorola.com>
2023-08-09 20:20:15 -0300

    Created UnorderedMapTricks.cpp

---
+++ Library/Miscellaneous/UnorderedMapTricks.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+unordered_map<long long, int, custom_hash> safe_map;
+gp_hash_table<long long, int, custom_hash> safe_hash_table;
+
+/*---------------another way-----------------*/
+/**
+ * Author: Simon Lindholm, chilli
+ * Date: 2018-07-23
+ * License: CC0
+ * Source: http://codeforces.com/blog/entry/60737
+ * Description: Hash map with mostly the same API as unordered\_map, but \tilde
+ * 3x faster. Uses 1.5x memory.
+ * Initial capacity must be a power of 2 (if provided).
+ */
+#pragma once
+
+#include <bits/extc++.h> /** keep-include */
+// To use most bits rather than just the lowest ones:
+struct chash { // large odd number for C
+    const uint64_t C = (long long)(4e18 * acos(0)) | 71;
+    long long operator()(long long x) const { return __builtin_bswap64(x*C); }
+};
+__gnu_pbds::gp_hash_table<long long,int,chash> h({},{},{},{},{1<<16});
+
+/* For CodeForces, or other places where hacking might be a problem:
+
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash { // To use most bits rather than just the lowest ones:
+    const uint64_t C = (long long)(4e18 * acos(0)) | 71; // large odd number
+    long long operator()(long long x) const { return __builtin_bswap64((x^RANDOM)*C); }
+};
+__gnu_pbds::gp_hash_table<long long, int, chash> h({},{},{},{}, {1 << 16});
+
+*/
+
+// pair<int, int> hash function
+struct HASH{
+  size_t operator()(const pair<int,int>&x)const{
+    return (size_t) x.first * 37U + (size_t) x.second;
+  }
+};
+unordered_map<int,int> mp;
+mp.reserve(1024);
+mp.max_load_factor(0.25);
+
+/*
+
+Links:
+
+https://codeforces.com/blog/entry/60737
+https://codeforces.com/blog/entry/62393
+
+*/
